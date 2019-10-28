52c40f45c5d574e30f2d6987feba3ef10e8c376b
Pierre Dal-Pra <dalpra.pierre@gmail.com>
2019-10-28 18:57:48 +0100

    Use LM APIs instead of parsing Ivy reports
    Motivation:
    
    Currently, the plugin is manually parsing Ivy reports files to extract
    its dependency tree to exclude Gatling and its transitive deps.
    This used to work until sbt 1.3 where Coursier is introduced as the new
    default dependency resolver, and Ivy reports are therefore not produced at all.
    We need to support both Ivy and Coursier resolutions to not overly
    restrain our users in their sbt builds.
    
    Modification:
    
    The manual Ivy report parsing has been replaced by calls to sbt's LM (library
    management) APIs, which abstracts over the underlying dependency
    resolver used by sbt.
    
    Results:
    
    sbt-frontline is now compatible with sbt 1.3 w/ Coursier, and no
    longer depends on sbt's implementation details around Ivy reports.

---
+++ src/main/scala/io/gatling/frontline/sbt/DependencyFilter.scala
@@ -0,0 +1,72 @@
+/**
+ * Copyright 2011-2019 GatlingCorp (http://gatling.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gatling.frontline.sbt
+
+import java.io.File
+
+import scala.annotation.tailrec
+import scala.collection.mutable
+
+import sbt.Keys.Classpath
+import sbt.internal.util.Attributed
+import sbt.librarymanagement._
+import sbt.util.Logger
+
+object DependencyFilter {
+
+  private val GatlingOrgs = Set("io.gatling", "io.gatling.highcharts", "io.gatling.frontline")
+
+  def nonGatlingDependencies(
+      resolution: DependencyResolution,
+      updateConfig: UpdateConfiguration,
+      unresolvedWarningConfig: UnresolvedWarningConfiguration,
+      config: Configuration,
+      logger: Logger,
+      rootModule: ModuleDescriptorConfiguration
+  ): Vector[File] = {
+    val moduleDescriptor = resolution.moduleDescriptor(rootModule)
+
+    val updateReport = resolution
+      .update(moduleDescriptor, updateConfig, unresolvedWarningConfig, logger)
+      .getOrElse(throw new IllegalStateException("Cannot build a fatjar with unresolved dependencies"))
+
+    val configurationReport = updateReport
+      .configuration(ConfigRef.configToConfigRef(config))
+      .getOrElse(throw new IllegalStateException(s"Could not find a report for configuration $config"))
+
+    val callers = moduleCallers(configurationReport.modules)
+    val excludedDeps = configurationReport.modules.filterNot(isTransitiveGatlingDependency(_, callers))
+
+    excludedDeps
+      .flatMap(_.artifacts)
+      .collect { case (artifact, file) if artifact.`type` == Artifact.DefaultType => file }
+  }
+
+  private def moduleCallers(reports: Vector[ModuleReport]): Map[ModuleID, List[ModuleID]] =
+    reports.map(report => (report.module.withConfigurations(None), report.callers.map(_.caller).toList)).toMap
+
+  private def isTransitiveGatlingDependency(report: ModuleReport, callers: Map[ModuleID, List[ModuleID]]): Boolean = {
+    @tailrec
+    def isTransitiveGatlingDependencyRec(toCheck: List[ModuleID]): Boolean =
+      toCheck match {
+        case Nil                                                => false
+        case dep :: _ if GatlingOrgs.contains(dep.organization) => true
+        case dep :: rest                                        => isTransitiveGatlingDependencyRec(callers.getOrElse(dep, Nil) ::: rest)
+      }
+
+    isTransitiveGatlingDependencyRec(List(report.module.withConfigurations(None)))
+  }
+}
--- src/main/scala/io/gatling/frontline/sbt/FrontLinePlugin.scala
+++ src/main/scala/io/gatling/frontline/sbt/FrontLinePlugin.scala
@@ -1,5 +1,5 @@
 /**
- * Copyright 2011-2018 GatlingCorp (http://gatling.io)
+ * Copyright 2011-2019 GatlingCorp (http://gatling.io)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,59 +15,54 @@
  */
 package io.gatling.frontline.sbt
 
-import java.io.File
+import io.gatling.frontline.sbt.FrontlineKeys._
 
 import sbt.Keys._
 import sbt._
 
+object FrontlineKeys {
+  val assembly = taskKey[File]("Builds a fatjar for FrontLine.")
+  val assemblyJarName = taskKey[String]("name of the fat jar")
+}
 object FrontLinePlugin extends AutoPlugin {
 
   override def requires = plugins.JvmPlugin
-
   override def trigger = allRequirements
 
-  object autoImport {
-    val assembly = taskKey[File]("Builds a fatjar for FrontLine.")
-    val assemblyIvyReport =
-      taskKey[File]("A task which returns the location of the ivy report file for test configuration.")
-    val assemblyJarName = taskKey[String]("name of the fat jar")
-  }
+  val autoImport = FrontlineKeys
 
-  import autoImport._
+  override lazy val projectSettings = frontlineSettings(Test)
 
-  override lazy val projectSettings: Seq[Def.Setting[_]] =
-    Seq(Compile, Test).flatMap(ivyReportForConfig)
-
-  private def ivyReportForConfig(config: Configuration) =
+  def frontlineSettings(config: Configuration) =
     inConfig(config)(
       Seq(
-        assemblyIvyReport := {
-          Def.task {
-            val ivyConfig =
-              ivyConfiguration.value.asInstanceOf[InlineIvyConfiguration]
-            val org = projectID.value.organization
-            val name = ivyModule.value.moduleSettings match {
-              case ic: InlineConfiguration => ic.module.name
-              case _ =>
-                throw new IllegalStateException("sbt-frontline plugin currently only supports InlineConfiguration of ivy settings (the default in sbt)")
-            }
-            new File(ivyConfig.resolutionCacheDir.get, s"reports/$org-$name-${config.name}.xml")
-
-          } dependsOn (compile in config)
-        }.value,
-        assembly := frontlineTask(config).value,
-        assemblyJarName in assembly := s"${moduleName.value}-frontline-${version.value}"
+        assembly := fatJarTask(config).value,
+        assemblyJarName := s"${moduleName.value}-frontline-${version.value}"
       )
     )
 
-  private def frontlineTask(config: Configuration): Def.Initialize[Task[File]] =
-    Def.task {
-      FrontLineFatJar.fatjar(
-        (assemblyIvyReport in config).value,
-        organization.value,
-        target.value,
-        (fullClasspath in config).value.files,
-        (assemblyJarName in assembly).value
-      )
+  private def fatJarTask(config: Configuration) = Def.task {
+    val moduleDescriptor = moduleDescriptorConfig.value
+
+    val dependencies = DependencyFilter.nonGatlingDependencies(
+      dependencyResolution.value,
+      updateConfiguration.value,
+      (unresolvedWarningConfiguration in update).value,
+      config,
+      streams.value.log,
+      moduleDescriptor
+    )
+
+    val classesDirectories = (fullClasspath in config).value.map(_.data).filter(_.isDirectory)
+
+    FatJar.packageFatJar(moduleDescriptor.module, classesDirectories, dependencies, target.value, assemblyJarName.value)
+  }
+
+  private def moduleDescriptorConfig = Def.task {
+    moduleSettings.value match {
+      case config: ModuleDescriptorConfiguration => config
+      case x =>
+        throw new IllegalStateException(s"sbt-frontline expected a ModuleDescriptorConfiguration, but got a ${x.getClass}")
     }
+  }
 }
